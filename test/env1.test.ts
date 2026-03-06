fbe76af8157f27e0afedb2ce13c841c67a477939
Matt Cox <matthew.a.cox@gmail.com>
2026-03-06 12:21:02 -0500

    test: remove temporary environment test files

--- test/env1.test.ts
+++
@@ -1,2 +0,0 @@
-import { test } from 'node:test';
-test('env1', () => { console.log('env1 pid:', process.pid); });
--- test/env2.test.ts
+++
@@ -1,2 +0,0 @@
-import { test } from 'node:test';
-test('env2', () => { console.log('env2 pid:', process.pid); });
